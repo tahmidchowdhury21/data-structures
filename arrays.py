8288f07e8b945852fc2c26604b2c952eb5bcec3b
Tahmid Chowdhury <tahmidchowdhury127@gmail.com>
2024-02-11 14:01:15 -0500

    added arrays

---
+++ arrays.py
@@ -0,0 +1,9 @@
+from array import array
+
+# This is creating an array in python
+my_array = array("i",[1,2,3,4,5,6,7,8])
+print(my_array)
+
+# Appending an element to the end of the array
+my_array.append(9)
+print(my_array)
